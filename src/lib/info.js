0f435a0a62fc24796f90ef2e406928bab2d133b1
0xbwill <71338748+0xbwill@users.noreply.github.com>
2023-08-28 14:09:08 +0200

    Update info.js

--- src/lib/info.js
+++ src/lib/info.js
@@ -3,7 +3,7 @@
 // feel free to remove this file.
 
 // URL of your website
-export const website = 'https://sveltekit-blog-template.vercel.app'
+export const website = 'https://svelteseo-f2i.netlify.app/'
 
 export const student1 = 'William'
 export const student2 = 'Nicolas'
@@ -14,13 +14,13 @@ export const name = `${student1} & ${student2}`
 // Used for the landing page & footer of posts
 export const avatar = '/avatar.jpg'
 export const bio = `\
-We're ${student1} & ${student2}, two aspiring computer science students hailing from Canada. William, a dedicated web developper, and Nicolas, an enthusiastic tech aficionado, share a common passion for the world of coding and technology.
+We're ${student1} & ${student2}, a pair of aspiring computer science students from Lille, France. We, William and Nicolas, are passionate about the realm of coding and technology. William is a dedicated web developer, while Nicolas is an enthusiastic tech aficionado.
 `
 
 // Your social usernames, empty strings won't be used.
 // For sake of the live demo, I'm using `username/repo` for github,
 // but you can replace with just your username.
-export const github = 'mattjennings/sveltekit-blog-template'
+export const github = '0xbwill/svelte-seo'
 export const twitter = ''
 export const linkedin = ''
 export const instagram = ''
