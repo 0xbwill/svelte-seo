fd29fa9df1be4d82e638b922c30b7371c380e28a
0xbwill <71338748+0xbwill@users.noreply.github.com>
2023-08-29 11:16:55 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,16 +17,13 @@
   }
 </script>
 
-<div class="flex flex-col min-h-screen ">
+<div class="flex flex-col min-h-screen">
   <div class="flex flex-col flex-grow w-full px-4 py-2">
     <header class="flex items-center justify-between w-full max-w-2xl py-4 mx-auto lg:pb-8">
-      <a
-        class="text-lg font-bold sm:text-2xl !text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-teal-600 dark:to-teal-400"
-        href="/"
-      >
-        {name}
-      </a>
-
+      <div class="d-flex justify-around">
+        <a class="text-2xl font-bold dark:text-white text-black mr-5" href="/">Accueil</a>
+        <a class="text-2xl font-bold dark:text-white text-black" href="/posts">Posts</a>
+      </div>
       <button
         type="button"
         role="switch"
